dcea6851635bd0843cd0c415e4957ddf2c97da1a
maneshkadam67 <maneshkadam67@gmail.com>
2025-06-23 04:06:02 +0530

    this is third commit

--- test_selenium_python.py
+++ test_selenium_python.py
@@ -13,7 +13,7 @@ def test_search_valid_product():
 
 
     options = webdriver.ChromeOptions()
-    options.add_argument('--headless')
+    #options.add_argument('--headless')
     options.add_argument('--disable-gpu')
     options.add_argument('--no-sandbox')
     options.add_argument('--disable-dev-shm-usage')
